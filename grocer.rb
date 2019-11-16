a91b534b15cf752ab78bc86f2b3cecc968cddf63
Sam Kyprianou <samuelkyprianou@gmail.com>
2019-11-16 22:57:44 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -25,6 +25,7 @@ def apply_coupons(cart, coupons)
       cart["#{item} W/COUPON"] = {price: coupon[:cost] / coupon[:num], clearance: cart[item][:clearance], count: coupon[:num]}
       cart[item][:count] -= coupon[:num]
     elsif coupon[:item][:count] >= coupon[:num] && cart["#{item} W/COUPON"]
+    
    end   
   end
 end
