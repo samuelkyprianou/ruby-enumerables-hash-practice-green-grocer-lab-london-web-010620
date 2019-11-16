8352a63e98df353bfd8bb1e8aca17aab0a35c3b8
Sam Kyprianou <samuelkyprianou@gmail.com>
2019-11-16 22:35:06 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -20,8 +20,8 @@ end
 
 def apply_coupons(cart, coupons)
   coupons.each do |coupon|
-    item = coupon[:item] && coupon[:item][:count] >= coupon[:num] && !cart["#{item} W/COUPON"]
-    if cart[item]
+    item = coupon[:item]
+    if cart[item] && coupon[:item][:count] >= coupon[:num] && !cart["#{item} W/COUPON"
   end
 end
 
